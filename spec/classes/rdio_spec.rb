a2415cc44fe915dbed4cc82535b656c91967d64b
Will Farrington <wfarr@github.com>
2012-10-17 14:58:02 -0700

    update specs and scripts

--- spec/classes/rdio_spec.rb
+++ spec/classes/rdio_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe 'rdio' do
-  it { should contain_package('Rdio') }
+  it do
+    should contain_package('Rdio').with({
+      :provider => 'appdmg',
+      :source   => 'http://www.rdio.com/media/static/desktop/mac/Rdio.dmg'
+    })
+  end
 end
